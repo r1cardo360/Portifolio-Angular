10c759d1b7998c7fc581efdcf6cff3e426b60061
r1cardo360 <ric.furlanetto@hotmail.com>
2025-03-05 04:26:26 -0300

    Adicionando o html inicial e adicionando images com os caminhos para as images. configurando o angular.json para receber assets

--- src/app/modules/portifolio/pages/home/home.component.ts
+++ src/app/modules/portifolio/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+//components
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
