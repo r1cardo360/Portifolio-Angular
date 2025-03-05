9df030a9e11b4630c6d29071cc91514356b7bb1d
r1cardo360 <ric.furlanetto@hotmail.com>
2025-03-05 02:45:56 -0300

    Resolvendo conflito

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+//components pages;
+import { HomeComponent } from './modules/portifolio/pages/home/home.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    }
+];
