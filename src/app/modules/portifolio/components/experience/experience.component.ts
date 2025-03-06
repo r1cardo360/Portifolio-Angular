29bccf2c18dbc8fddb12c15e0aff3833403c65ab
r1cardo360 <ric.furlanetto@hotmail.com>
2025-03-06 15:40:44 -0300

    Adicionando style css na página e nos components

--- src/app/modules/portifolio/components/experience/experience.component.ts
+++ src/app/modules/portifolio/components/experience/experience.component.ts
@@ -20,17 +20,13 @@ export class ExperienceComponent {
       },
       text: `<p><strong>Função:</strong> Desenvolvedor Web (Promoção após atuar como Suporte Técnico)</p>
         <p>Minha trajetória nesta empresa iniciou como suporte técnico, onde minhas responsabilidades incluíam:</p>
-        <ul>
-            <li>Auxiliar usuários com problemas relacionados ao sistema, incluindo manutenção e instalação.</li>
-            <li>Instalar banco de dados e prestar suporte via WhatsApp e telefone.</li>
-            <li>Desenvolver e atualizar relatórios para clientes utilizando SQL, PostgreSQL e FastReport.</li>
-            <li>Realizar manutenção em hardware e instalar servidores Linux (Fedora 39, 40 e 41).</li>
-        </ul>
+        <p>Auxiliar usuários com problemas relacionados ao sistema, incluindo manutenção e instalação.
+        Instalar banco de dados e prestar suporte via WhatsApp e telefone.
+        Desenvolver e atualizar relatórios para clientes utilizando SQL, PostgreSQL e FastReport.
+        Realizar manutenção em hardware e instalar servidores Linux (Fedora 39, 40 e 41).</p>
         <p>Atualmente, fui promovido a Desenvolvedor Web, assumindo novas responsabilidades, como:</p>
-        <ul>
-            <li>Desenvolvimento utilizando Node.js, HTML, CSS, JavaScript, jQuery e Bootstrap.</li>
-            <li>Participação na análise de migração do front-end para Angular com a equipe.</li>
-        </ul>`
+            <p>Desenvolvimento utilizando Node.js, HTML, CSS, JavaScript, jQuery e Bootstrap.
+            Participação na análise de migração do front-end para Angular com a equipe.</p>`
     },
     {
       sumary:{
@@ -38,12 +34,10 @@ export class ExperienceComponent {
         p: 'Lavive Maio 2022 - Dez 2023'
       },
       text: `<p>Durante meu período na Lavive, fui responsável por:</p>
-        <ul>
-            <li>Realizar manutenção em mais de 100 máquinas, tanto na parte de hardware quanto de software.</li>
-            <li>Instalar o banco de dados e o sistema da empresa.</li>
-            <li>Auxiliar usuários com problemas nas máquinas.</li>
-            <li>Ajudar na infraestrutura e cabeamento dos setores.</li>
-        </ul>`
+            <p>Realizar manutenção em mais de 100 máquinas, tanto na parte de hardware quanto de software.
+            Instalar o banco de dados e o sistema da empresa.
+            Auxiliar usuários com problemas nas máquinas.
+            Ajudar na infraestrutura e cabeamento dos setores.</p>`
     }
   ])
 }
